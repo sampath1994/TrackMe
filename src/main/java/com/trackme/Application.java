e498c31ad5af9fff26af0128cef82e187267cee5
promod94 <webpromod@gmail.com>
2018-04-05 16:29:20 +0530

    initialize project
    pom updated. application class created and directory structure created

---
+++ src/main/java/com/trackme/Application.java
@@ -0,0 +1,16 @@
+package com.trackme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by promod on 4/5/2018.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}
