8c87a177b984abada6ff55fa07f8514fa5aa916a
promod94 <webpromod@gmail.com>
2018-07-04 00:37:54 +0530

    First commit

--- src/main/java/com/trackme/controller/webController.java
+++ src/main/java/com/trackme/controller/webController.java
@@ -1,7 +1,17 @@
 package com.trackme.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by promod on 4/5/2018.
  */
+@Controller
 public class webController {
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String mainPage()
+    {
+        return "index";
+    }
 }
